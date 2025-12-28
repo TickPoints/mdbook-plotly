82cbb09b8e9a056300258735c21abd9b6c407406
Tick Points <tickpoints@outlook.com>
2025-12-28 14:44:09 +0800

    Fix comments

--- src/macros.rs
+++ src/macros.rs
@@ -1,5 +1,5 @@
 //! Used to send critical errors.
-//! Will exit directly with exit code 1.
+//! Will exit directly with exit code `1`.
 #[macro_export]
 macro_rules! fatal {
     ($($arg:tt)*) => {{
