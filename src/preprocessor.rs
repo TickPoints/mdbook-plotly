9b01bb509476c992359b94542f4564d29fd3db4e
Tick Points <tickpoints@outlook.com>
2025-12-28 16:57:52 +0800

    Adjust preprocessor
    - Use rayon

--- src/preprocessor.rs
+++ src/preprocessor.rs
@@ -4,8 +4,8 @@ mod handlers;
 
 use crate::fatal;
 
-#[cfg(feature = "js-tool")]
-mod js_tool;
+#[cfg(feature = "js-tools")]
+mod js_tools;
 
 pub fn preprocess_book() {
     let mut book_data = bookdata::get_book_data();
@@ -13,8 +13,20 @@ pub fn preprocess_book() {
     book_data.emit_compatibility_warning();
 
     let config = book_data.get_config();
-    for chapter in book_data.chapter_iter_mut() {
-        handlers::handle(chapter, &config);
+    #[cfg(feature = "js-tools")]
+    {
+        use rayon::prelude::*;
+
+        book_data
+            .chapter_iter_mut()
+            .par_iter()
+            .for_each(|chapter| handlers::handle(chapter, &config));
+    }
+    #[cfg(not(feature = "js-tools"))]
+    {
+        book_data
+            .chapter_iter_mut()
+            .for_each(|chapter| handlers::handle(chapter, &config));
     }
 
     let (ctx, book) = book_data.into_parts();
