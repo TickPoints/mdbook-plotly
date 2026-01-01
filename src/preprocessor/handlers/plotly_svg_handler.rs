b401a8e420fcbf3bec70f158bf2cf2b88dc32fd8
Tick Points <tickpoints@outlook.com>
2026-01-01 11:21:27 +0800

    Adjust function signatures

--- src/preprocessor/handlers/plotly_svg_handler.rs
+++ src/preprocessor/handlers/plotly_svg_handler.rs
@@ -1,12 +1,13 @@
 use crate::preprocessor::js_tools;
 use pulldown_cmark::Event;
 
-pub fn handle(code: String) -> Result<Event<'_>, js_tools::QuickjsError> {
+pub fn handle(code: String) -> Result<Event<'static>, js_tools::QuickjsError> {
     js_tools::with_js_runtime(|runtime| {
         let context = js_tools::get_sandboxed_context(runtime)?;
-        context.with(|local_ctx| {
+        context.with(|local_ctx| -> Result<(), js_tools::QuickjsError> {
             let plotly = local_ctx.eval(include_str!("plotly.js"))?;
             eprintln!("code: {code}");
+            Ok(plotly)
         })?;
         Ok(Event::Text("empty".into()))
     })
