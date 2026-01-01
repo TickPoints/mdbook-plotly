cdddf3d5d938112b3564b1dda19d5e57e4c6df94
Tick Points <tickpoints@outlook.com>
2026-01-01 12:02:52 +0800

    Adjust tool interfaces
    - Adjust `js_tools`
    	- Add `eval_script`, `to_js_optional_string`

--- src/preprocessor/js_tools.rs
+++ src/preprocessor/js_tools.rs
@@ -1,6 +1,6 @@
 use once_cell::sync::Lazy;
 pub use rquickjs::Error as QuickjsError;
-use rquickjs::{Context, Runtime};
+use rquickjs::{Context, Ctx, FromJs, Runtime};
 #[cfg(not(feature = "sync"))]
 use std::rc::Rc as SupportRc;
 #[cfg(feature = "sync")]
@@ -16,6 +16,9 @@ static GLOBAL_JS_RUNTIME: Lazy<SupportRc<Runtime>> = Lazy::new(|| {
     SupportRc::new(rt)
 });
 
+/// The interface receives a closure. The closure will be able to take the global runtime and use it.
+///
+/// NOTE: The obtained Runtime is Rcized, and the form of Rc depends on the `sync` feature.
 pub fn with_js_runtime<F, R>(f: F) -> R
 where
     F: FnOnce(&Runtime) -> R,
@@ -23,7 +26,53 @@ where
     f(GLOBAL_JS_RUNTIME.as_ref())
 }
 
+/// The interface allows a Runtime to be used to generate a sandboxed context.
+///
+/// As other features are refined, the sandbox will also be tighter.
+/// Therefore, we do not recommend using JS features and global quantities that are not explicitly supported in other documents.
+/// Because they may be silently deleted in the minor version.
 pub fn get_sandboxed_context(rt: &Runtime) -> Result<Context, QuickjsError> {
     let context = Context::base(rt)?;
     Ok(context)
+}
+
+pub enum ScriptError {
+    JsError(String),
+    InternalError(QuickjsError),
+}
+
+/// Converts an optional string value into its JavaScript string representation.
+/// Returns `"undefined"` if the value is `None`, otherwise returns the string value.
+pub fn to_js_optional_string<T>(value: T) -> String
+where
+    T: Into<Option<String>>,
+{
+    value.into().unwrap_or_else(|| "undefined".to_string())
+}
+
+/// It is a wrapper around the `eval` function of the original `Ctx`.
+/// It is convenient to collect JS errors at the same time.
+pub fn eval_script<'js, V: FromJs<'js>, S: Into<Vec<u8>>>(
+    ctx: &Ctx<'js>,
+    source: S,
+) -> Result<V, ScriptError> {
+    let eval_result = ctx.eval::<V, _>(source);
+    match eval_result {
+        Err(rquickjs::Error::Exception) => {
+            let formatted_exception = ctx
+                .catch()
+                .as_exception()
+                .map(|exception| {
+                    format!(
+                        "{}\n{}",
+                        to_js_optional_string(exception.message()),
+                        to_js_optional_string(exception.stack())
+                    )
+                })
+                .unwrap_or_else(|| String::from("Undefined exception."));
+            Err(ScriptError::JsError(formatted_exception))
+        }
+        Err(e) => Err(ScriptError::InternalError(e)),
+        Ok(v) => Ok(v),
+    }
 }
