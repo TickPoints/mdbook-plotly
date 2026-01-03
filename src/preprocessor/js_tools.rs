7133226b93b36745efbef943ee237846629fdb9e
Tick Points <tickpoints@outlook.com>
2026-01-03 13:16:03 +0800

    Adjust tool interfaces
    - Adjust `js_tools`
    	- Add `inject_readonly`
    	- Adjust `get_sandboxed_context`

--- src/preprocessor/js_tools.rs
+++ src/preprocessor/js_tools.rs
@@ -1,11 +1,69 @@
 use once_cell::sync::Lazy;
 pub use rquickjs::Error as QuickjsError;
-use rquickjs::{Context, Ctx, FromJs, Runtime};
+use rquickjs::{Context, Ctx, FromJs, IntoAtom, IntoJs, Object, Runtime, object::Accessor};
+// basic
+
+#[derive(Debug)]
+pub enum ScriptError {
+    ScriptError(String),
+    InterError(QuickjsError),
+}
+
+use std::error::Error;
+impl Error for ScriptError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        if let Self::InterError(inter_error) = self {
+            Some(inter_error)
+        } else {
+            None
+        }
+    }
+}
+
+use std::fmt;
+impl fmt::Display for ScriptError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::ScriptError(e) => write!(f, "Js Tools Error: Script Error: {}", e),
+            Self::InterError(e) => write!(f, "Js Tools Error: Inter Error: {:?}", e),
+        }
+    }
+}
+
+impl From<QuickjsError> for ScriptError {
+    fn from(v: QuickjsError) -> Self {
+        Self::InterError(v)
+    }
+}
+
+/// Converts an optional string value into its JavaScript string representation.
+/// Returns `"undefined"` if the value is `None`, otherwise returns the string value.
+pub fn to_js_optional_string<T>(value: T) -> String
+where
+    T: Into<Option<String>>,
+{
+    value.into().unwrap_or_else(|| "undefined".to_string())
+}
+
+pub fn inject_readonly<'a, 'js, K, V>(
+    obj: &'a Object<'js>,
+    key: K,
+    v: V,
+) -> Result<(), QuickjsError>
+where
+    K: IntoAtom<'js>,
+    V: IntoJs<'js> + Clone + 'js,
+{
+    let value = v.clone();
+    obj.prop(key, Accessor::new_get(move || value.clone()))
+}
+
+// practical
+
 #[cfg(not(feature = "sync"))]
 use std::rc::Rc as SupportRc;
 #[cfg(feature = "sync")]
 use std::sync::Arc as SupportRc;
-
 static GLOBAL_JS_RUNTIME: Lazy<SupportRc<Runtime>> = Lazy::new(|| {
     let rt = Runtime::new().unwrap_or_else(|e| {
         crate::fatal_in_init!(
@@ -32,22 +90,14 @@ where
 /// Therefore, we do not recommend using JS features and global quantities that are not explicitly supported in other documents.
 /// Because they may be silently deleted in the minor version.
 pub fn get_sandboxed_context(rt: &Runtime) -> Result<Context, QuickjsError> {
-    let context = Context::base(rt)?;
+    let context = Context::full(rt)?;
+    context.with(|ctx| -> Result<(), QuickjsError> {
+        // Inject content
+        let globals = ctx.globals();
+        use rquickjs::function::Func;
+        Ok(())
+    })?;
     Ok(context)
-}
-
-pub enum ScriptError {
-    JsError(String),
-    InternalError(QuickjsError),
-}
-
-/// Converts an optional string value into its JavaScript string representation.
-/// Returns `"undefined"` if the value is `None`, otherwise returns the string value.
-pub fn to_js_optional_string<T>(value: T) -> String
-where
-    T: Into<Option<String>>,
-{
-    value.into().unwrap_or_else(|| "undefined".to_string())
 }
 
 /// It is a wrapper around the `eval` function of the original `Ctx`.
@@ -70,9 +120,9 @@ pub fn eval_script<'js, V: FromJs<'js>, S: Into<Vec<u8>>>(
                     )
                 })
                 .unwrap_or_else(|| String::from("Undefined exception."));
-            Err(ScriptError::JsError(formatted_exception))
+            Err(ScriptError::ScriptError(formatted_exception))
         }
-        Err(e) => Err(ScriptError::InternalError(e)),
+        Err(e) => Err(ScriptError::InterError(e)),
         Ok(v) => Ok(v),
     }
 }
