25e898bd80bcb460ac40a82c2ab4562d2bf8d29f
Tick Points <tickpoints@outlook.com>
2026-02-01 17:59:55 +0800

    Optimize `plotly_svg_handler.rs`

--- src/preprocessor/handlers/plotly_svg_handler.rs
+++ src/preprocessor/handlers/plotly_svg_handler.rs
@@ -1,16 +1,12 @@
 use crate::preprocessor::js_tools;
 use pulldown_cmark::Event;
 
-#[allow(clippy::let_unit_value)]
 pub fn handle(code: String) -> Result<Event<'static>, js_tools::ScriptError> {
     js_tools::with_js_runtime(|runtime| {
         let context = js_tools::get_sandboxed_context(runtime)?;
         context.with(|local_ctx| -> Result<(), js_tools::ScriptError> {
-            // NOTE: The seemingly useless binding is a must here.
-            // It can avoid being wrongly evaluated as `!`.
-            let plotly = js_tools::eval_script(&local_ctx, include_str!("plotly.js"))?;
-            log::error!("code: {code}");
-            Ok(plotly)
+            js_tools::eval_script::<(), _>(&local_ctx, include_str!("plotly.js"))?;
+            Ok(())
         })?;
         Ok(Event::Text("empty".into()))
     })
