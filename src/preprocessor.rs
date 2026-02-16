13c17c29e1141d62425e6738e3e8f8e0596a9be2
Tick Points <tickpoints@outlook.com>
2026-02-16 16:34:47 +0800

    Adjust preprocessor
    - Adjust `book_data`
    - Update `handler`
    - Update `plotly_svg_handler`

--- src/preprocessor.rs
+++ src/preprocessor.rs
@@ -10,18 +10,19 @@ pub fn preprocess_book() {
     book_data.emit_compatibility_warning();
 
     let config = book_data.get_config();
+    let book_path = book_data.get_book_path();
     #[cfg(feature = "sync")]
     {
         use rayon::prelude::*;
         book_data
             .chapter_par_iter()
-            .for_each(|chapter| handlers::handle(chapter, &config));
+            .for_each(|chapter| handlers::handle(chapter, &config, &book_path));
     }
     #[cfg(not(feature = "sync"))]
     {
         book_data
             .chapter_iter_mut()
-            .for_each(|chapter| handlers::handle(chapter, &config));
+            .for_each(|chapter| handlers::handle(chapter, &config, &book_path));
     }
 
     let preprocessed_book = book_data.into_book();
