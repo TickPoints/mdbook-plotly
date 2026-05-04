26d598b385f5420dceb4b971928ccbba7fb0d6ba
TickPoints <tickpoints@outlook.com>
2026-05-04 10:52:09 +0800

    Update interfaces
    - Add Marker in `bar`, `histogra`, `pie`, `scatter`, `scatter_geo`, `scatter_mapbox`, `scatter_polar`

--- src/preprocessor/handlers/code_handler/plot_obj_parser/pie_parser.rs
+++ src/preprocessor/handlers/code_handler/plot_obj_parser/pie_parser.rs
@@ -1,4 +1,4 @@
-use super::until::{Map, must_translate};
+use super::until::{Color, Map, must_translate};
 use crate::{translate, translate_enum};
 use anyhow::Result;
 use plotly::Pie;
@@ -46,6 +46,59 @@ pub fn parse_pie_data(pie_obj: &mut serde_json::Value, map: &Map) -> Result<Box<
             "counterclockwise" =>   PieDirection::CounterClockwise,
         }),
     }?;
+
+    let pie = if let Some(marker_obj) = pie_obj.get_mut("marker")
+        && marker_obj.is_object()
+    {
+        let marker = plotly::common::Marker::new();
+        let marker = translate! {
+            marker,
+            marker_obj,
+            map,
+            (color, Color),
+            (opacity, f64),
+            (size, usize),
+            (size_array, Vec<usize>),
+            (max_displayed, usize),
+            (size_ref, usize),
+            (size_min, usize),
+            (cauto, bool),
+            (cmax, f64),
+            (cmin, f64),
+            (cmid, f64),
+            (auto_color_scale, bool),
+            (reverse_scale, bool),
+            (show_scale, bool),
+            (outlier_color, Color)
+        }?;
+
+        use plotly::common::{MarkerSymbol, SizeMode};
+        let marker = translate_enum! {
+            marker,
+            marker_obj,
+            map,
+            (symbol, {
+                "circle" =>         MarkerSymbol::Circle,
+                "square" =>         MarkerSymbol::Square,
+                "diamond" =>        MarkerSymbol::Diamond,
+                "cross" =>          MarkerSymbol::Cross,
+                "x" =>              MarkerSymbol::X,
+                "triangle-up" =>    MarkerSymbol::TriangleUp,
+                "triangle-down" =>  MarkerSymbol::TriangleDown,
+                "triangle-left" =>  MarkerSymbol::TriangleLeft,
+                "triangle-right" => MarkerSymbol::TriangleRight,
+                "pentagon" =>       MarkerSymbol::Pentagon,
+                "hexagon" =>        MarkerSymbol::Hexagon,
+            }),
+            (size_mode, {
+                "area" => SizeMode::Area,
+                "diameter" => SizeMode::Diameter,
+            }),
+        }?;
+        pie.marker(marker)
+    } else {
+        pie
+    };
 
     Ok(pie)
 }
--- src/preprocessor/handlers/code_handler/plot_obj_parser/scatter_polar_parser.rs
+++ src/preprocessor/handlers/code_handler/plot_obj_parser/scatter_polar_parser.rs
@@ -1,6 +1,6 @@
-use super::until::{Map, must_translate};
+use super::until::{Color, Map, must_translate};
 use crate::{translate, translate_enum};
-use anyhow::Result;
+use anyhow::{Ok, Result};
 use plotly::ScatterPolar;
 
 pub fn parse_scatter_polar_data(
@@ -64,6 +64,59 @@ pub fn parse_scatter_polar_data(
             "legendonly" => Visible::LegendOnly,
         }),
     }?;
+
+    let sp = if let Some(marker_obj) = sp_obj.get_mut("marker")
+        && marker_obj.is_object()
+    {
+        let marker = plotly::common::Marker::new();
+        let marker = translate! {
+            marker,
+            marker_obj,
+            map,
+            (color, Color),
+            (opacity, f64),
+            (size, usize),
+            (size_array, Vec<usize>),
+            (max_displayed, usize),
+            (size_ref, usize),
+            (size_min, usize),
+            (cauto, bool),
+            (cmax, f64),
+            (cmin, f64),
+            (cmid, f64),
+            (auto_color_scale, bool),
+            (reverse_scale, bool),
+            (show_scale, bool),
+            (outlier_color, Color)
+        }?;
+
+        use plotly::common::{MarkerSymbol, SizeMode};
+        let marker = translate_enum! {
+            marker,
+            marker_obj,
+            map,
+            (symbol, {
+                "circle" =>         MarkerSymbol::Circle,
+                "square" =>         MarkerSymbol::Square,
+                "diamond" =>        MarkerSymbol::Diamond,
+                "cross" =>          MarkerSymbol::Cross,
+                "x" =>              MarkerSymbol::X,
+                "triangle-up" =>    MarkerSymbol::TriangleUp,
+                "triangle-down" =>  MarkerSymbol::TriangleDown,
+                "triangle-left" =>  MarkerSymbol::TriangleLeft,
+                "triangle-right" => MarkerSymbol::TriangleRight,
+                "pentagon" =>       MarkerSymbol::Pentagon,
+                "hexagon" =>        MarkerSymbol::Hexagon,
+            }),
+            (size_mode, {
+                "area" => SizeMode::Area,
+                "diameter" => SizeMode::Diameter,
+            }),
+        }?;
+        sp.marker(marker)
+    } else {
+        sp
+    };
 
     Ok(sp)
 }
